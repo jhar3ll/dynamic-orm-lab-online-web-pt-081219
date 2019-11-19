fc4705bf5d05ced46fc07c6049c4515bcbc021ab
Jharell Chambers <jharell.chambers@selu.edu>
2019-11-19 00:56:44 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -36,9 +36,13 @@ class InteractiveRecord
   
    def values_for_insert
     values = []
+    
     self.class.column_names.each do |col_name|
       values << "'#{send(col_name)}'" unless send(col_name).nil?
     end
-    values.join(", ")
+    return values.join(", ")
   end
+  
+  def save 
+    
 end

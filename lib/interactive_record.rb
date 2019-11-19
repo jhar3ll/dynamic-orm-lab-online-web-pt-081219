0790865b22a4815e786db9df3db929ff41a79583
Jharell Chambers <jharell.chambers@selu.edu>
2019-11-19 01:08:34 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -43,8 +43,8 @@ class InteractiveRecord
     return values.join(", ")
   end
   
-  def save(values_for_insert)
-    DB[:conn].execute("INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES ?", [values_for_insert]) #(#{values_for_insert})")
+  def save 
+    DB[:conn].execute("INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})")
   
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
   end
